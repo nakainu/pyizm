98c0568539b78f1b09afdaeddc8d6ac4ada270c7
nakainu <c011619379@edu.teu.ac.jp>
2020-03-16 00:32:02 +0900

    Understand the various types of methods

---
+++ application/type_method.py
@@ -0,0 +1,18 @@
+# メソッド一覧
+
+class TestClass:
+
+    # インスタンスメソッド
+    def sample_instancemethod(self, arg_1):
+        pass
+
+    # クラスメソッド
+    @classmethod
+    def samnple_classmethod(cls, arg_1):
+        pass
+
+    # スタティックメソッド
+    @staticmethod
+    def sample_statocmethod(arg_1, arg_2):
+        pass
+
